718b83e1c0c8a928520a5b06cd50bbab4233cb18
Akinimaginable <akn@etrange.org>
2025-09-30 16:11:26 +0200

    Add tests

--- Tests/swift-names-generatorTests/namesGeneratorTests.swift
+++ Tests/swift-names-generatorTests/namesGeneratorTests.swift
@@ -1,6 +1,30 @@
 import Testing
+
 @testable import NamesGenerator
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test func testGetRandomName() async throws {
+    for _ in 0..<50 {
+        let name = GetRandomName()
+
+        #expect(!name.isEmpty)
+        #expect(name.contains("_"))
+
+        let parts: [String.SubSequence] = name.split(separator: "_")
+        #expect(
+            parts.count == 2,
+            "Name should consist of two parts separated by an underscore"
+        )
+
+        let leftPart = String(parts[0])
+        let rightPart = String(parts[1])
+
+        #expect(
+            left.contains(leftPart),
+            "The adjective should exist in the 'left' array"
+        )
+        #expect(
+            right.contains(rightPart),
+            "The surname should exist in the 'right' array"
+        )
+    }
 }
